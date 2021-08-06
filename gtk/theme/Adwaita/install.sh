a2bdf3267f46d88025cafe030daa65fcfbfd078e
stzups <griffinht@gmail.com>
2021-08-05 21:34:15 -0400

    add install script

---
+++ gtk/theme/Adwaita/install.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# https://askubuntu.com/a/1170167
+sudo apt-get -y install sassc
+./parse-sass.sh
+sudo mkdir -p /usr/share/themes/AdwaitaPink/gtk-3.0/
+sudo ln -s $(pwd)/gtk-contained.css /usr/share/themes/AdwaitaPink/gtk-3.0/gtk.css
+sudo ln -s $(pwd)/assets /usr/share/themes/AdwaitaPink/gtk-3.0/assets
