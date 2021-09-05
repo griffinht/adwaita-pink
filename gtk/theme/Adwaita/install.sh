08724e331d6883545bef31afd2b3c21a34fa73d1
stzups <griffinht@gmail.com>
2021-09-04 22:46:15 -0400

    update install.sh

--- gtk/theme/Adwaita/install.sh
+++ gtk/theme/Adwaita/install.sh
@@ -1,8 +1,18 @@
 #!/bin/bash
+set -e
+
+if [ ! -z "$1" ]; then
+    echo "using given working directory"
+    directory="$1"
+else
+    echo "warning: using current working directory"
+    directory=$(pwd)
+fi
+echo "$directory"
 
 # https://askubuntu.com/a/1170167
 sudo apt-get -y install sassc
-./parse-sass.sh
+"$directory/parse-sass.sh"
 sudo mkdir -p /usr/share/themes/AdwaitaPink/gtk-3.0/
-sudo ln -s $(pwd)/gtk-contained.css /usr/share/themes/AdwaitaPink/gtk-3.0/gtk.css
-sudo ln -s $(pwd)/assets /usr/share/themes/AdwaitaPink/gtk-3.0/assets
+sudo ln -s "$directory/gtk-contained.css /usr/share/themes/AdwaitaPink/gtk-3.0/gtk.css"
+sudo ln -s "$directory/assets /usr/share/themes/AdwaitaPink/gtk-3.0/assets"
